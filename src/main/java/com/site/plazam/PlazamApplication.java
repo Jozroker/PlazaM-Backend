9211389470eb224be6829bf01c2de0866c4ad274
Jozroker <rostykzadyraichuk010@gmail.com>
2021-05-16 12:52:51 +0300

    Set project settings

---
+++ src/main/java/com/site/plazam/PlazamApplication.java
@@ -0,0 +1,13 @@
+package com.site.plazam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlazamApplication.class, args);
+	}
+
+}
